98df25880fce217d048136e0f2b2882e742278c8
Faustharus <chailloleau-damien@outlook.fr>
2024-08-09 10:47:47 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Damien Chailloleau on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

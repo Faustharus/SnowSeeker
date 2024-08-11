2fe869dde1f09c35c8d3546ef8d18495361d5703
Faustharus <chailloleau-damien@outlook.fr>
2024-08-11 10:54:45 +0200

    Initial Commit

---
+++ SnowSeeker/GroupView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupView.swift
+//  SnowSeeker
+//
+//  Created by Damien Chailloleau on 11/08/2024.
+//
+
+import SwiftUI
+
+struct GroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupView()
+}
